a6e0ae1c853ad72c4ec4017aee52096c7d337390
Dan <daniel.speranskiy@live.com>
2023-12-18 09:11:57 +0400

    Added the aside bar

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -6,6 +6,8 @@ import { getServerSession } from 'next-auth'
 import { optionsAuth } from '@/app/api/auth/[...nextauth]/route'
 import Image from 'next/image'
 import Link from 'next/link'
+import LogOut from '@/components/btns/LogOut'
+import { redirect } from 'next/navigation'
 
 export const metadata = {
   title: 'Make-a-Link',
@@ -22,21 +24,33 @@ export default async function RootLayout({ children }) {
   <html lang="en">
       <body className={inter.className}>
           <main className='flex min-h-screen '>
-            <aside className='bg-indigo-100 max-w-[200px] grow p-4'>
+            <aside className='bg-white max-w-[200px] grow p-4 shadow-lg'>
               <div className='rounded-full overflow-hidden aspect-square w-20 mx-auto'>
                 <Image src={session?.user?.image} alt={session?.user?.name} width={256} height={256} />
               </div>
-              <nav>
-                <Link href='/account'>
+              <nav className='flex flex-col justify-center text-center mt-8 gap-4 mx-auto '>
+                <Link href='/account' className='flex gap-2 font-semibold hover:bg-blue-50 rounded-lg p-2 transition duration-300 ease-in'>
+                  <Image src='/settings.svg' alt='Settings' width={25} height={25} />
                   Settings
                 </Link>
-                <Link href='/analytics'>
+                <Link href='/analytics' className='flex gap-2 font-semibold hover:bg-blue-50 rounded-lg p-2 transition duration-300 ease-in'>
+                  <Image src='/analytics.svg' alt='Analytics' width={25} height={25} />
                   Analytics
                 </Link>
+                <Link href='/' className='flex gap-2 font-semibold items-center hover:bg-blue-50 rounded-lg p-2 transition duration-300 ease-in'>
+                  <Image src='/house.svg' alt='Analytics' width={30} height={30} />
+                  Home
+                </Link>
+                <div className='flex gap-2  bottom-5 absolute items-center font-semibold hover:bg-blue-50 rounded-lg p-1 transition duration-300 ease-in w-[170px]'>
+                <LogOut className='w-full z-10' width={35} height={35} />
+                <span className='bottom-[10px] absolute left-10'>Log Out</span>
+                </div>
               </nav>
             </aside>
-            <div className=' max-w-7xl mx-auto p-8 md:px-16' >
-            {children}
+            <div className=' grow ' >
+              <div className='bg-white m-4 p-4 shadow-md rounded-lg '>
+              {children}
+              </div>
             </div>
           </main>
         </body>
--- src/components/btns/LogOut.js
+++ src/components/btns/LogOut.js
@@ -3,15 +3,19 @@ import React from 'react'
 import Image from 'next/image'
 import { signOut } from 'next-auth/react'
 
-const LogOut = () => {
+const LogOut = ({
+  className='group flex items-center gap-1 group-hover:transition transition-2 duration-300 ease-in group-hover:ease-in group-hover:duration-300',
+  width=25,
+  height=25
+}) => {
   return (
    <button
    onClick={() => {
        signOut()
    }}
-   className='group flex items-center gap-1 group-hover:transition transition-2 duration-300 ease-in group-hover:ease-in group-hover:duration-300'
+   className={className}
    >
-    <Image src='/logout.svg' alt='Log Out' width={25} height={25} className=' rotate-180 hover:transition group-hover:ease-in group-hover:duration-300 ' />
+    <Image src='/logout.svg' alt='Log Out' width={width} height={height} className=' rotate-180 hover:transition group-hover:ease-in group-hover:duration-300 ' />
     <span className='hidden hover:transition group-hover:ease-in group-hover:duration-300 group-hover:flex group-hover:text-black'>Log Out</span>
    </button>
   )
